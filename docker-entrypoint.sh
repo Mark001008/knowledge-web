9cd489ee59e47dd33ab667c5fc7ace952104c2b4
mark <mhchao1008@gmail.com>
2026-07-03 16:08:12 +0800

    Proxy API through web container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-: "${KB_API_BASE_URL:=http://localhost:8080}"
+: "${KB_API_BASE_URL:=}"
 
 cat > /usr/share/nginx/html/config.js <<EOF
 window.KB_API_BASE_URL = "${KB_API_BASE_URL}";
--- public/config.js
+++ public/config.js
@@ -1,1 +1,1 @@
-window.KB_API_BASE_URL = window.KB_API_BASE_URL || "http://localhost:8080";
+window.KB_API_BASE_URL = window.KB_API_BASE_URL || "";
